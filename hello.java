a5617b89c154f2a5abefe8bec7615810bde29685
roiyal <roiyalmilktea123456789@gmail.com>
2022-04-27 16:25:38 +0900

    changed

--- hello.java
+++ hello.java
@@ -1,7 +1,12 @@
 class hello {
-	public static void main(Strings[] args) {
-		System.out.println("hello");
+	public static void main(String[] args) {
+		int i;
+		int sum;
+		sum = 0;
+		for (i = 0; i < 5; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
 
 	}
-
 }
